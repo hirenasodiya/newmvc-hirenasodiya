261426e01769b8582220baea1af18eef9435f39c
Hiren Asodiya <hirenasodiya4444@gmail.com>
2023-04-13 14:11:28 +0530

    eav change in customer

--- Block/Eav/Attribute/Edit.php
+++ Block/Eav/Attribute/Edit.php
@@ -17,4 +17,15 @@ class Block_Eav_Attribute_Edit extends Block_Core_Template
 		$attribute = $this->getData('attribute');
 		return $attribute;
 	}
+
+	public function getAttributeOption()
+	{
+		$attributeId = Ccc::getModel('Core_Request')->getParam('attribute_id');
+		$sql = "SELECT * FROM `eav_attribute_option` WHERE `attribute_id` = $attributeId";
+		$attributeOption = Ccc::getModel('Eav_Attribute_Option')->fetchAll($sql);
+		// echo "<pre>";
+		// print_r($attributeOption);
+		// die();
+		return $attributeOption;
+	}
 }
